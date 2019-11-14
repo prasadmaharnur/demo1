328942e0bac1361b1b7232e34223121bff64e3ce
prasadmaharnur <57747150+prasadmaharnur@users.noreply.github.com>
2019-11-14 00:06:15 -0800

    Create Addition.java
    this is for addition in java

---
+++ Addition.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Addition
+{
+    public static void main(String[] args)
+    {
+        int a,b;
+        a=10;
+        b=20;
+        
+        System.out.print(a+b);
+        
+    }
+}
